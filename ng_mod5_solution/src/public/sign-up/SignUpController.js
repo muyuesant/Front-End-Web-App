9d73d7ec17f75b8079c467188d527a2cbccfa059
xiangteng chen <muyuesanter@gmail.com>
2023-04-22 18:02:29 -0400

    mod 10 task 3

--- ng_mod5_solution/src/public/sign-up/SignUpController.js
+++ ng_mod5_solution/src/public/sign-up/SignUpController.js
@@ -11,12 +11,13 @@
       signCtrl.user = {};
       signCtrl.allMenu = allMenu;
       signCtrl.foundDish = false;
-      signCtrl.error = false;
+      signCtrl.saved = false;
 
       signCtrl.submit = function (){
-        console.log("signCtrl.user: ",signCtrl.user);
-        console.log("UserService.user: ",UserService.getUser());
+        // console.log("signCtrl.user: ",signCtrl.user);
+        // console.log("UserService.user: ",UserService.getUser());
         signCtrl.foundDish = false;
+        signCtrl.saved = false;
         var cate = signCtrl.user.menuNumber.replace(/\d/g,"");
         if (cate.length) {
             var menu = signCtrl.allMenu[cate.toUpperCase()];
@@ -24,7 +25,6 @@
                 var found = menu.menu_items.find(element => element.short_name === signCtrl.user.menuNumber.toUpperCase());
                 if (found) {
                     signCtrl.foundDish = true;
-                    signCtrl.error = false;
                     signCtrl.user.favorite = found;
                     signCtrl.user.cate = cate.toUpperCase();
                 }
@@ -32,50 +32,41 @@
 
             }     
         }
-        if (signCtrl.foundDish == false) {
-            signCtrl.error = true;
-        }
-        else {
-            console.log("signCtrl.user: ",signCtrl.user);
-            
+        if (signCtrl.foundDish) {
+            // console.log("signCtrl.user: ",signCtrl.user);
             UserService.setUser(signCtrl.user);
-            console.log("UserService.user: ",UserService.getUser());
+            signCtrl.saved = true;
+            // console.log("UserService.user: ",UserService.getUser());
         }
-        // console.log("signCtrl.foundDish: ",signCtrl.foundDish);
-        // console.log("signCtrl.error: ",signCtrl.error);
-        // console.log(signCtrl.user);
 
       };
       
     }
 
-    MenuNumberDirective.$inject = ['MenuService','$q'];
-    function MenuNumberDirective(MenuService,$q){
+    MenuNumberDirective.$inject = ['MenuService'];
+    function MenuNumberDirective(MenuService){
         var ddo = {
             require: 'ngModel',
             link: function(scope, elm, attrs, ctrl) {
                 ctrl.$validators.menuNumber = function(modelValue, viewValue) {
-
-                    if(ctrl.$touched) {
-                        var allMenu= MenuService.getAllMenuItems();
-                        console.log("modelValue",modelValue)
-                        // console.log("allMenu",allMenu)
-                        if (modelValue) {
-                            var cate = modelValue.replace(/\d/g,"");
-                            var menu = allMenu[cate.toUpperCase()];
-                            // console.log("menu",menu);
-                            if (menu) {
-                                var found = menu.menu_items.find(element => element.short_name === modelValue.toUpperCase());
-                                // console.log("found",found);
-                                if (found) {
-                                    return true;
-                                }
-                            }     
-                        }
-                        // console.log("somthing wrong")
-                        return false
+                    var allMenu= MenuService.getAllMenuItems();
+                    console.log("modelValue",modelValue)
+                    // console.log("allMenu",allMenu)
+                    if (modelValue) {
+                        var cate = modelValue.replace(/\d/g,"");
+                        var menu = allMenu[cate.toUpperCase()];
+                        // console.log("menu",menu);
+                        if (menu) {
+                            var found = menu.menu_items.find(element => element.short_name === modelValue.toUpperCase());
+                            // console.log("found",found);
+                            if (found) {
+                                return true;
+                            }
+                        }     
                     }
-                    return true;
+                    // console.log("somthing wrong")
+                    return false
+                   
                 };                 
             }
         };
